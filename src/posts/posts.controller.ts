70436b01f8116ac3a91200e2f652e3f448059ec0
lee gyu yeol-mac <i1004gy@gmail.com>
2025-04-30 19:39:28 +0900

    서비스 코드 작성

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -10,67 +10,20 @@ import {
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
-interface PostsModel {
-  id: number;
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
-let posts: PostsModel[] = [
-  {
-    id: 1,
-    author: 'newjeans_official',
-    title: 'New Jeans',
-    content: 'New Jeans is a South Korean',
-    likeCount: 1000,
-    commentCount: 100,
-  },
-  {
-    id: 2,
-    author: 'newjeans_official',
-    title: '뉴진스 해린',
-    content: 'New Jeans is a South Korean',
-    likeCount: 1000,
-    commentCount: 100,
-  },
-  {
-    id: 3,
-    author: 'blackpink_official',
-    title: '블랙핑크 리사',
-    content: 'New Jeans is a South Korean',
-    likeCount: 1000,
-    commentCount: 100,
-  },
-  {
-    id: 4,
-    author: 'blackpink_official',
-    title: '블랙핑크 제니',
-    content: 'New Jeans is a South Korean',
-    likeCount: 1000,
-    commentCount: 100,
-  },
-];
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
   // GET /posts
   // 모든 post를 가져온다
   @Get()
-  getPosts(): PostsModel[] {
-    return posts;
+  getPosts() {
+    return this.postsService.getAllPosts();
   }
   // 2)GET /posts/:id
   // 특정 id를 가진 post를 가져온다
   @Get(':id')
   getPost(@Param('id') id: string) {
-    const post = posts.find((post) => post.id === Number(id));
-    if (!post) {
-      throw new NotFoundException();
-    }
-    return post;
+    return this.postsService.getPostById(Number(+id));
   }
   // 3)POST /posts
   // 새로운 post를 생성한다
@@ -80,16 +33,7 @@ export class PostsController {
     @Body('title') title: string,
     @Body('content') content: string,
   ) {
-    const post = {
-      id: posts[posts.length - 1].id + 1,
-      author,
-      title,
-      content,
-      likeCount: 0,
-      commentCount: 0,
-    };
-    posts = [...posts, post];
-    return posts;
+    return this.postsService.createPost(author, title, content);
   }
   // 4)PUT /posts/:id
   // 특정 id를 가진 post를 수정한다
@@ -100,34 +44,12 @@ export class PostsController {
     @Body('title') title?: string,
     @Body('content') content?: string,
   ) {
-    const post = posts.find((post) => post.id === Number(id));
-    if (!post) {
-      throw new NotFoundException();
-    }
-    if (author) {
-      post.author = author;
-    }
-    if (title) {
-      post.title = title;
-    }
-    if (content) {
-      post.content = content;
-    }
-    posts = posts.map((prevPost) =>
-      prevPost.id === Number(id) ? post : prevPost,
-    );
-    return posts;
+    return this.postsService.updatePost(+id, author, title, content);
   }
   // 5)DELETE /posts/:id
   // 특정 id를 가진 post를 삭제한다
   @Delete(':id')
   deletePosts(@Param('id') id: string) {
-    const post = posts.find((post) => post.id === Number(id));
-    if (!post) {
-      throw new NotFoundException();
-    }
-    posts = posts.filter((post) => post.id !== Number(id));
-
-    return id;
+    return this.postsService.deletePost(+id);
   }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,103 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+export interface PostsModel {
+  id: number;
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+let posts: PostsModel[] = [
+  {
+    id: 1,
+    author: 'newjeans_official',
+    title: 'New Jeans',
+    content: 'New Jeans is a South Korean',
+    likeCount: 1000,
+    commentCount: 100,
+  },
+  {
+    id: 2,
+    author: 'newjeans_official',
+    title: '뉴진스 해린',
+    content: 'New Jeans is a South Korean',
+    likeCount: 1000,
+    commentCount: 100,
+  },
+  {
+    id: 3,
+    author: 'blackpink_official',
+    title: '블랙핑크 리사',
+    content: 'New Jeans is a South Korean',
+    likeCount: 1000,
+    commentCount: 100,
+  },
+  {
+    id: 4,
+    author: 'blackpink_official',
+    title: '블랙핑크 제니',
+    content: 'New Jeans is a South Korean',
+    likeCount: 1000,
+    commentCount: 100,
+  },
+];
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  getAllPosts() {
+    return posts;
+  }
+  getPostById(id: number) {
+    const post = posts.find((post) => post.id === Number(id));
+    if (!post) {
+      throw new NotFoundException();
+    }
+    return post;
+  }
+  createPost(author: string, title: string, content: string) {
+    const post = {
+      id: posts[posts.length - 1].id + 1,
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0,
+    };
+    posts = [...posts, post];
+    return posts;
+  }
+  updatePost(
+    postId: number,
+    author?: string,
+    title?: string,
+    content?: string,
+  ) {
+    const post = posts.find((post) => post.id === Number(postId));
+    if (!post) {
+      throw new NotFoundException();
+    }
+    if (author) {
+      post.author = author;
+    }
+    if (title) {
+      post.title = title;
+    }
+    if (content) {
+      post.content = content;
+    }
+    posts = posts.map((prevPost) =>
+      prevPost.id === Number(postId) ? post : prevPost,
+    );
+    return posts;
+  }
+  deletePost(postId: number) {
+    const post = posts.find((post) => post.id === Number(postId));
+    if (!post) {
+      throw new NotFoundException();
+    }
+    posts = posts.filter((post) => post.id !== Number(postId));
+
+    return postId;
+  }
+}
